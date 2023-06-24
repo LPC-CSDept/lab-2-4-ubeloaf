02c4da8c111bf176186dd238aac4bad265357b04
ubeloaf <136755601+ubeloaf@users.noreply.github.com>
2023-06-24 00:09:30 +0000

    solution

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:]
+    merged_str = sub2 + ' ' + sub1
     print(sub2)
     print(sub1)
     print(merged_str)
